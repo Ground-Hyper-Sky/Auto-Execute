2c0775ba9ee9d8f6f3d91ef80ac807238480e875
GHS_Official <84139787+GHS-Official@users.noreply.github.com>
2024-02-20 20:09:41 +0800

    Update AutoExecute_Language.py
    未测试代码

--- auto_execute/AutoExecute_Language.py
+++ auto_execute/AutoExecute_Language.py
@@ -1,8 +1,10 @@
 import time
+import datetime
+from dateutil.relativedelta import relativedelta
 
 
 class Auto_Language:
-    Language_list = ["sleep", "loop"]
+    Language_list = ["sleep", "loop", "tell"]
 
     @classmethod
     def analysis_command(cls, command, source, script):
@@ -24,6 +26,7 @@ class Auto_Language:
 
         return False,
 
+    # @ae sleep <time>
     @staticmethod
     def sleep(command, source, script):
         try:
@@ -35,6 +38,7 @@ class Auto_Language:
             source.reply(f"§4§l脚本{script}命令{command.split()[2]}运行出错。错误信息:\n§e{e}")
             return False
 
+    # @ae loop <time>
     @staticmethod
     def loop(command, source, script):
         try:
@@ -45,3 +49,160 @@ class Auto_Language:
         except Exception as e:
             source.reply(f"§4§l脚本{script}命令{command.split()[2]}运行出错。错误信息:\n§e{e}")
             return False
+
+    # @ae tell <obj> <con>
+    @staticmethod
+    def tell(command, source, script):
+
+        pass
+
+    # @ae timed %Y-%m-%d %H:%M:%S
+    @staticmethod
+    def timed(command, source, script):
+        i = command.split(maxsplit=2)[-1]
+
+        if not (len(i.split()) == 2 and len(i.split()[0].split(sep="-")) == 3 and len(
+                i.split()[1].split(sep=":")) == 3):
+            return
+
+        d = i.split()[0].split(sep="-")
+        t = i.split()[1].split(sep=":")
+
+        def get_date_obj(date, _time):
+            try:
+                return datetime.datetime.strptime(i, "%Y-%m-%d %H:%M:%S")
+
+            except ValueError:
+                pass
+
+            if date[0] == "YY":
+                if date[1] == "mm":
+                    if date[2] == "dd":
+                        if datetime.datetime.now() < datetime.datetime.now().replace(hour=int(_time[0]), minute=int(_time[1]),
+                                                                                     second=int(_time[2]), microsecond=0):
+                            return datetime.datetime.now().replace(hour=int(_time[0]), minute=int(_time[1]), second=int(_time[2]),
+                                                                   microsecond=0)
+
+                        return datetime.datetime.now().replace(hour=int(_time[0]), minute=int(_time[1]), second=int(_time[2]),
+                                                               microsecond=0) + datetime.timedelta(days=1)
+
+                    if int(date[2]) > datetime.datetime.now().day:
+                        return datetime.datetime.now().replace(day=int(date[2]), hour=int(_time[0]), minute=int(_time[1]),
+                                                               second=int(_time[2]), microsecond=0) + relativedelta(month=1)
+
+                    if int(date[2]) == datetime.datetime.now().day:
+                        if datetime.datetime.now() < datetime.datetime.now().replace(hour=int(_time[0]), minute=int(_time[1]),
+                                                                                     second=int(_time[2]), microsecond=0):
+                            return datetime.datetime.now().replace(hour=int(_time[0]), minute=int(_time[1]), second=int(_time[2]),
+                                                                   microsecond=0)
+
+                        return datetime.datetime.now().replace(day=int(date[2]), hour=int(_time[0]), minute=int(_time[1]),
+                                                               second=int(_time[2]), microsecond=0) + relativedelta(month=1)
+
+                    return datetime.datetime.now().replace(hour=int(_time[0]), minute=int(_time[1]), second=int(_time[2]),
+                                                           microsecond=0)
+
+                if date[2] != "dd":
+                    if datetime.datetime.now() < datetime.datetime.now().replace(month=int(date[1]), day=int(date[2]),
+                                                                                 hour=int(_time[0]), minute=int(_time[1]),
+                                                                                 second=int(_time[2]), microsecond=0):
+                        return datetime.datetime.now().replace(month=int(date[1]), day=int(date[2]), hour=int(_time[0]),
+                                                               minute=int(_time[1]), second=int(_time[2]), microsecond=0)
+
+                    return datetime.datetime.now().replace(month=int(date[1]), day=int(date[2]), hour=int(_time[0]),
+                                                           minute=int(_time[1]),
+                                                           second=int(_time[2]), microsecond=0) + relativedelta(year=1)
+
+                if int(date[1]) > datetime.datetime.now().month:
+                    return datetime.datetime.now().replace(month=int(date[1]), day=1, hour=int(_time[0]), minute=int(_time[1]),
+                                                           second=int(_time[2]), microsecond=0) + relativedelta(year=1)
+
+                if int(date[1]) == datetime.datetime.now().month:
+                    if datetime.datetime.now() < datetime.datetime.now().replace(hour=int(_time[0]), minute=int(_time[1]),
+                                                                                 second=int(_time[2]), microsecond=0):
+                        return datetime.datetime.now().replace(hour=int(_time[0]), minute=int(_time[1]), second=int(_time[2]),
+                                                               microsecond=0)
+
+                    return datetime.datetime.now().replace(hour=int(_time[0]), minute=int(_time[1]), second=int(_time[2]),
+                                                           microsecond=0) + datetime.timedelta(days=1)
+
+                return datetime.datetime.now().replace(month=int(date[1]), day=1, hour=int(_time[0]), minute=int(_time[1]),
+                                                       second=int(_time[2]), microsecond=0)
+
+            elif date[1] == "mm":
+
+                if date[2] == "dd":
+                    if int(date[0]) > datetime.datetime.now().year:
+                        return datetime.datetime.now().replace(year=int(date[0]), month=1, day=1, hour=int(_time[0]),
+                                                               minute=int(_time[1]), second=int(_time[2]), microsecond=0)
+
+                    if int(date[0]) == datetime.datetime.now().year:
+                        if datetime.datetime.now() < datetime.datetime.now().replace(hour=int(_time[0]), minute=int(_time[1]),
+                                                                                     second=int(_time[2]), microsecond=0):
+                            return datetime.datetime.now().replace(hour=int(_time[0]), minute=int(_time[1]), second=int(_time[2]),
+                                                                   microsecond=0)
+
+                        return datetime.datetime.now().replace(hour=int(_time[0]), minute=int(_time[1]), second=int(_time[2]),
+                                                               microsecond=0) + datetime.timedelta(days=1)
+                    return
+
+                if int(date[0]) > datetime.datetime.now().year:
+                    return datetime.datetime.now().replace(year=int(date[0]), month=1, day=1, hour=int(_time[0]),
+                                                           minute=int(_time[1]), second=int(_time[2]), microsecond=0)
+
+                if int(date[0]) == datetime.datetime.now().year:
+                    if int(date[2]) > datetime.datetime.now().day:
+                        return datetime.datetime.now().replace(day=int(date[2]), hour=int(_time[0]),
+                                                               minute=int(_time[1]), second=int(_time[2]), microsecond=0)
+
+                    if int(date[2]) == datetime.datetime.now().day:
+                        if datetime.datetime.now() < datetime.datetime.now().replace(hour=int(_time[0]), minute=int(_time[1]),
+                                                                                     second=int(_time[2]), microsecond=0):
+                            return datetime.datetime.now().replace(hour=int(_time[0]), minute=int(_time[1]), second=int(_time[2]),
+                                                                   microsecond=0)
+
+                        return datetime.datetime.now().replace(hour=int(_time[0]), minute=int(_time[1]), second=int(_time[2]),
+                                                               microsecond=0) + relativedelta(month=1)
+
+                    return datetime.datetime.now().replace(hour=int(_time[0]), minute=int(_time[1]), second=int(_time[2]),
+                                                           microsecond=0) + relativedelta(month=1)
+
+                return
+
+            elif date[2] == "dd":
+                if int(date[0]) > datetime.datetime.now().year:
+                    return datetime.datetime.now().replace(year=int(date[0]), month=int(date[1]), day=1, hour=int(_time[0]),
+                                                           minute=int(_time[1]), second=int(_time[2]),
+                                                           microsecond=0)
+
+                if int(date[0]) == datetime.datetime.now().year:
+                    if int(date[1]) > datetime.datetime.now().month:
+                        return datetime.datetime.now().replace(month=int(date[1]), day=1, hour=int(_time[0]),
+                                                               minute=int(_time[1]), second=int(_time[2]),
+                                                               microsecond=0)
+
+                    if int(date[1]) == datetime.datetime.now().month:
+                        if datetime.datetime.now() < datetime.datetime.now().replace(hour=int(_time[0]),
+                                                                                     minute=int(_time[1]), second=int(_time[2]),
+                                                                                     microsecond=0):
+                            return datetime.datetime.now().replace(hour=int(_time[0]),
+                                                                   minute=int(_time[1]), second=int(_time[2]),
+                                                                   microsecond=0)
+
+                        return datetime.datetime.now().replace(hour=int(_time[0]),
+                                                               minute=int(_time[1]), second=int(_time[2]),
+                                                               microsecond=0) + datetime.timedelta(days=1)
+
+                    return
+
+                return
+
+            return
+
+        if get_date_obj(d,t):
+            pass
+
+
+    @staticmethod
+    def copy(command, source, script):
+        pass
